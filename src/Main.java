8e5eac1b3fbee59b68bd629c74f0329057d5c4ca
Celso20 <pv22469@alunos.estgv.ipv.pt>
2022-05-03 12:30:30 +0100

    boa noite

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        tap.ProjetoFinal.GestorPassageiro g = new tap.ProjetoFinal.GestorPassageiro();
+        GestorPassageiro g = new GestorPassageiro();
         Scanner sc = new Scanner(System.in);
         String op, nif, curso, ficheiro;
         int tipo;
