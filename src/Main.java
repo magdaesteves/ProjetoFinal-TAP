4c24155de85210345de2ab686c5c2d6add7b1d96
magdaesteves <magdasfesteves@gmail.com>
2022-05-10 17:15:04 +0100

    magda

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         while (op.equals("0") == false) {
             switch (op) {
                 case "1":
-                    System.out.println("1-Escolha o tipo de Pessoa 1-Passageiro 2-Assistente de Bordo");
+                    System.out.println("Escolha o tipo de pessoa 1-Passageiro 2-Assistente de Bordo");
                     tipo = sc.nextInt();
                     if (tipo == 1) {
                         g.addPassageiro(tipo);
@@ -25,7 +25,7 @@ public class Main {
                                     gA.lerRotaTxt("rotas.txt");
                                     break;
                                 case "2":
-                                    System.out.println("Qual o id da Rota que pretende listar os voos:");
+                                    System.out.println("Qual o id da rota que pretende listar os voos:");
                                     idRota = sc.nextInt();
                                     gA.listarVoosPorRota("voos.txt", idRota);
                                     break;
@@ -33,14 +33,14 @@ public class Main {
                                     gA.lerPassageiroNomeIdTxt("passageiros.txt");
                                     break;
                                 case "4":
-                                    System.out.println("Qual o id do Voo que pretende listar todos os passageiros:");
+                                    System.out.println("Qual o id do voo que pretende listar todos os passageiros:");
                                     idVoo = sc.nextInt();
                                     gA.lerPassageiroNomeIdTxtPorVoo("passageiros.txt",idVoo);
                                     break;
                                 case "5":
                                     break;
                                 case "6":
-                                    System.out.println("Qual o id do Passageiro que pretende listar os bilhetes:");
+                                    System.out.println("Qual o id do passageiro que pretende listar os bilhetes:");
                                     idPassageiro = sc.next();
                                     gA.lerBilheteTxtPorPassageiro("bilhetes.txt", idPassageiro);
                                     break;
@@ -58,11 +58,11 @@ public class Main {
     private static String menu() {
         String op;
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n#---MENU PRINCIPAL---------------------------------------------------------------#");
-        System.out.println("|  (1) - Bem vindo à companhia aérea DWDM AirViseu deseja continuar? 1-Sim 0-Não |");
-        System.out.println("|  (2) - Ler ficheiro do passageiro                                              |");
-        System.out.println("|  (0) - Sair                                                                    |");
-        System.out.println("#--------------------------------------------------------------------------------#");
+        System.out.println("\n#---------------------------------------------------------------------#");
+        System.out.println("|  Bem vindo à companhia aérea DWDM AirViseu deseja continuar?        |");
+        System.out.println("|  (1) - Sim                                                          |");
+        System.out.println("|  (0) - Não                                                          |");
+        System.out.println("#---------------------------------------------------------------------#");
         System.out.print("Escolha uma opção: ");
 
         op=sc.next();
